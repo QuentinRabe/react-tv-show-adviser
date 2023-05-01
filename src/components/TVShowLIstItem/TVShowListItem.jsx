5329f474a2ec452f59fec29325288fba2795b8b7
QuentinRabe <lovegoodandyh@gmail.com>
2023-05-01 12:14:31 +0200

    first commit

---
+++ src/components/TVShowLIstItem/TVShowListItem.jsx
@@ -0,0 +1,13 @@
+import { SMALL_IMG_COVER_BASE_URL } from "../../config";
+import s from "./style.module.css";
+export function TVShowListItem({ tvShow, onClick }) {
+  return (
+    <div onClick={() => onClick(tvShow)} className={s.container}>
+      <img
+        src={SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path}
+        className={s.img}
+      />
+      <div className={s.title}>{tvShow.name}</div>
+    </div>
+  );
+}
